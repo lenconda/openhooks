e02c307403154dd5fb16b048034af19351cd1181
lenconda <1053464288@qq.com>
2019-04-14 16:09:57 +0800

    🐛 fix: init script

---
+++ scripts/init.ts
@@ -0,0 +1,30 @@
+import pathExists from 'path-exists'
+import fs from 'fs'
+import {
+  openhooksDir,
+  keysFile,
+  routersFile } from '../src/utils/constants'
+
+const initializeFIle = path => {
+  fs.writeFileSync(path, JSON.stringify([]), { encoding: 'utf-8' })
+  return
+}
+
+if (!pathExists.sync(openhooksDir)) {
+  try {
+    fs.mkdirSync(openhooksDir, { recursive: true })
+    initializeFIle(keysFile)
+    initializeFIle(routersFile)
+  } catch (e) {
+    throw e
+  }
+} else {
+  try {
+    if (!pathExists.sync(keysFile))
+      initializeFIle(keysFile)
+    if (!pathExists.sync(routersFile))
+      initializeFIle(routersFile)
+  } catch (e) {
+    throw e
+  }
+}
